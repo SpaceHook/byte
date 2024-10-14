bc2f52ac55c7c08dff1eebebabe84563bcf8dd68
SpaceHook <dumiakm@gmail.com>
2024-10-14 21:41:18 +0300

    save

--- config/export.php
+++ config/export.php
@@ -2,72 +2,40 @@
 
 return [
 
-    /*
-     * If true, the exporter will crawl through your site's pages to determine
-     * the paths that need to be exported.
-     */
     'crawl' => true,
-    'base_url' => 'http://127.0.0.1:8000', // або порт, на якому працює ваш Laravel-сервер
+'base_url' => 'http://127.0.0.1:8000', // або порт, на якому працює ваш Laravel-сервер
 
-    /*
-     * Add additional paths to be added to the export here. If you're using the
-     * `crawl` option, you probably don't need to add anything here.
-     *
-     * For example: "about", "posts/featured"
-     */
-    'paths' => [],
+'paths' => [],
 
-    /*
-     * Files and folders that should be included in the build. Expects
-     * key/value pairs with current paths as keys, and destination paths
-     * as values.
-     *
-     * By default your `public` folder's contents will be added to the export.
-     */
-    'include_files' => [
-        'public' => '',
-    ],
+'include_files' => [
+'public' => '',
+],
 
-    /*
-     * File patterns that should be excluded from the included files.
-     */
-    'exclude_file_patterns' => [
-        '/\.php$/',
-        '/mix-manifest\.json$/',
-    ],
+/*
+* Додайте цей блок коду для включення статичних ресурсів (CSS, JS, зображень, build).
+*/
+'assets' => [
+'build', // додайте цю стрічку, щоб включити файли Vite
+'css',
+'js',
+'images',
+],
 
-    /*
-     * Whether or not the destination folder should be emptied before starting
-     * the export.
-     */
-    'clean_before_export' => true,
+'exclude_file_patterns' => [
+'/\.php$/',
+'/mix-manifest\.json$/',
+],
 
-    /*
-     * If set, the site will be exported to this disk. Disks can be configured
-     * in `config/filesystems.php`.
-     *
-     * If empty, your site will be exported to a `dist` folder.
-     */
-    'disk' => null,
+'clean_before_export' => true,
 
-    /*
-     * Shell commands that should be run before the export starts when running
-     * `php artisan export`.
-     *
-     * You can skip these by adding a `--skip-{name}` flag to the command.
-     */
-    'before' => [
-        // 'assets' => '/usr/local/bin/yarn production',
-    ],
+'disk' => null,
 
-    /*
-     * Shell commands that should be run after the export has finished when
-     * running `php artisan export`.
-     *
-     * You can skip these by adding a `--skip-{name}` flag to the command.
-     */
-    'after' => [
-        // 'deploy' => '/usr/local/bin/netlify deploy --prod',
-    ],
+'before' => [
+// 'assets' => '/usr/local/bin/yarn production',
+],
+
+'after' => [
+// 'deploy' => '/usr/local/bin/netlify deploy --prod',
+],
 
 ];
